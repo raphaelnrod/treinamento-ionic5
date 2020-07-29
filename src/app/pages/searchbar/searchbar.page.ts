ba2fdcff666ea1fb3971646a34aa8d371d0bf52d
Raphael Augusto <raphaelnrod@gmail.com>
2020-07-28 22:57:15 -0300

    fiz um searchbar fodidooooooooo

--- src/app/pages/searchbar/searchbar.page.ts
+++ src/app/pages/searchbar/searchbar.page.ts
@@ -7,9 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchbarPage implements OnInit {
 
-  constructor() { }
+  name : string = '';
+  notFound : string;
+  people : any = [];
+  filteredPeople : any;
+  nomes : any = ['Raphael', 'Carlos', 'Jean', 'Maria', 'Joaquim', 'Queren', 'Mariana', 'Vanessa', 'Manoel', 'Leticia'];
 
-  ngOnInit() {
+  private getRandomInt(min, max){
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  constructor() {
+    for (let index = 1; index <= 10; index++) {
+      this.people.push({
+        id: index, 
+        age: this.getRandomInt(10, 80), 
+        name: this.nomes[index - 1]
+      });
+    }
+    this.filteredPeople = this.people;    
+  }
+  
+  ngOnInit() { }
+
+  filterName(event){
+    this.name = event.target.value.toLowerCase();
+    this.filteredPeople = this.filterPerson(this.name);
+    this.filteredPeople.length == 0 ? this.notFound = "Nada foi encontrado" : this.notFound = null;
+  }
+
+  filterPerson(name){
+    this.filteredPeople = this.people;
+    return this.filteredPeople.filter((person) =>{
+      return person.name.toLowerCase().includes(name.toLowerCase());
+    })
   }
 
 }
